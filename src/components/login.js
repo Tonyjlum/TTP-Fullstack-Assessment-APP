6da0ac9f7fa2bf3f06f1429bad5c10de0bc0a0fc
Tonyjlum <tonyjlum@gmail.com>
2019-09-30 01:07:11 -0400

    first

---
+++ src/components/login.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  state = {
+    email: "",
+    password: "",
+}
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+  }
+
+  render() {
+    return (
+      <div className="center">
+        <form
+          className= "login-form"
+          onChange={this.handleChange}
+          onSubmit={this.handleSubmit}>
+          <h2>Sign In</h2>
+          <input
+            className="form-input"
+            id="email"
+            placeholder="Email"
+            value={this.state.email}
+            required/>
+          <br/>
+          <input
+            className="form-input"
+            id="password"
+            placeholder="password"
+            value={this.state.password}
+            required/>
+          <br/>
+          <input type="submit" value="Login"/>
+        </form>
+      </div>
+    );
+  }
+
+}
+
+export default Login;
